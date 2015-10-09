abb82b1feb2b999df2009a2fb08f653e00427d04
DevonTheNovice <devon.t.st.john@gmail.com>
2015-10-09 10:24:49 -0400

    Cleanup of deprecated codes
    Removed deprecated codes that are planed to be moved to other modules in version 0.0.5 ALPHA

--- unet.lua
+++ unet.lua
@@ -5,11 +5,10 @@ local fs = require("filesystem")
 local serialization = require("serialization")
 
 local unet = {}
-unet.comm = {}
 
 unet.info = {   --version info, used in preloading and version control DO NOT EDIT 
 ["os_build"] = "OpenOS 1.5",
-["version"] = "0.0.4 ALPHA",
+["version"] = "0.0.5 ALPHA",
 ["allowOutdated"] = false,
 ["mods"] = "/unet/mods"
 }
@@ -19,66 +18,41 @@ unet.info = {   --version info, used in preloading and version control DO NOT ED
 --uses the computer addr as a replacement name if the var does not exist
 
 function unet.getName()
-  if _DEVICENAME then return _DEVICENAME 
-  else return string.sub(computer.address(),1,5).."..."
+  if _DEVICENAME then 
+    return _DEVICENAME 
+  else return 
+    string.sub(computer.address(),1,5).."..."
   end
 end
+
+--usually called by other functions in other modules, this is used to determine
+--the first available interface, and returns it's id.
 
 function unet.resolveID()
   for for i=1,#unet.driver.inter do
     if unet.driver.inter[i] and unet.driver.inter[i].isAvailable then 
-	  return i
-	end
+	    return i
+	  end
   end
   return false
 end
+
+--version checker and module loader, looks for a file with the argument as it's name
+--then checks to see if it is already loaded, or if it has is valid for loading, ie
+--built for this version of unet and this OS. it loads it and returns true, or an error
+--message if it could not load.
 
 function unet.loadMod(name)
   if unet[name] then
     return true,"Mod already loaded into memory"
   elseif fs.exists(unet.info.mods.."/"..name..".lua") then
     dofile(unet.info.mods.."/"..name..".lua")
-	if unet[name] then
-	  return true,"Mod loaded into unet"
-	else
-	  return false,"Mod failed to load"
-	end
+	  if unet[name] then
+	    return true,"Mod loaded into unet"
+	  else
+	    return false,"Mod failed to load"
+	  end
   end
-end
-
---a basic function that sends a message to parent devices requesting more
---information about them, this also signals the start of functions that
---a user will actually use normally
-
---[[function unet.comm.scanForParent(faddr,id)
-  if not id then id = resolveID() end
-  if id = false or not unet.driver.inter[id].isAvailable then
-    return false, "Device not found"
-  end
-  if faddr then
-    unet.driver.inter[id].usend(faddr,"data0","UNET_PARENT_SCAN",unet.getName())
-	return true
-  else
-    unet.driver.inter[id].ubroadcast("data0","UNET_PARENT_SCAN",unet.getName())
-	return true
-  end
-end]]
-
---does what you'd expect, tries to connect to a parent device. the actual
---connection is automatic, if the connection is approved by the parent
---the device will connect automatically, an event is pushed when a response
---to this occures regardless
-
-function unet.comm.connectToParent(id,addr,pass,user)
-  if not unet.driver.inter[id].isAvailable then
-    return false, "Device not found"
-  end
-  local data = serialization.serialize({["name"]=unet.getName(),["pass"]=pass or "",["user"]=user or ""})
-  unet.driver.inter[id].usend(addr,"data0","UNET_PARENT_CONN_REQUEST",data)
-  unet.driver.inter[id].parent = addr
-  unet.driver.inter[id].pass = pass or ""
-  unet.driver.inter[id].user = user or ""
-  return true
 end
 
 return unet
