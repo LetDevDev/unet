4cb279f22eff6370e2f25bbac1a15387320ee396
DevonTheNovice <devon.t.st.john@gmail.com>
2015-10-14 11:41:43 -0400

    Update arp.lua

--- arp.lua
+++ arp.lua
@@ -3,7 +3,7 @@ local event = require("event")
 local pack = require("serialization")
 local unet = require("unet")
 
-unet.routes = {}
+unet.arp.routes = {}
 
 --arp addressing system unet, in some applications this will not
 --be as useful, but generally a device will want to have at least
@@ -12,6 +12,14 @@ unet.routes = {}
 --address format is in the form of numbers split by slashes
 --1 is a valid address
 --4/2543/2522/33/442/88/9 is also a valid address (must be a big network to get this though!)
+
+function unet.arp.getAddress(id)
+  if unet.driver.
+end
+
+function unet.arp.checkAddr(addr)
+  
+end
 
 function unet.arp.scan(id,type,filter)
   if not id then
