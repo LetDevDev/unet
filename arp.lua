1fd4ef7d21e64bc5f2cfdc9d441d17f211880298
DevonTheNovice <devon.t.st.john@gmail.com>
2015-10-14 12:01:30 -0400

    Update arp.lua

--- arp.lua
+++ arp.lua
@@ -5,16 +5,17 @@ local unet = require("unet")
 
 unet.arp.routes = {}
 
---arp addressing system unet, in some applications this will not
---be as useful, but generally a device will want to have at least
---one arp destination.
+--arp addressing system unet, used for addressing in both managed and unmanaged networks.
 
---address format is in the form of numbers split by slashes
+--address format is in the form of numbers split by slashes, all numbers before the final slash
+--are subnet addresses used for routing. take the address 2/3/4 
 --1 is a valid address
+--0 is not a valid address, it is a valid subnetwork though:
+--0/1 is a valid address
 --4/2543/2522/33/442/88/9 is also a valid address (must be a big network to get this though!)
 
 function unet.arp.getAddress(id)
-  if unet.driver.
+  if unet.driver.inter[id]
 end
 
 function unet.arp.checkAddr(addr)
