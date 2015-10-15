71d5a7cbec8b8824dd52ac56c79bfc31b1f63bc6
DevonTheNovice <devon.t.st.john@gmail.com>
2015-10-15 10:48:47 -0400

    Update arp.lua

--- arp.lua
+++ arp.lua
@@ -8,14 +8,19 @@ unet.arp.routes = {}
 --arp addressing system unet, used for addressing in both managed and unmanaged networks.
 
 --address format is in the form of numbers split by slashes, all numbers before the final slash
---are subnet addresses used for routing. take the address 2/3/4 
+--are subnet addresses used for routing. Taking the address 2/3/4 we can figure out this device is
+--on a subnetwork named 3, and that network is on another network: 2
 --1 is a valid address
 --0 is not a valid address, it is a valid subnetwork though:
 --0/1 is a valid address
 --4/2543/2522/33/442/88/9 is also a valid address (must be a big network to get this though!)
 
 function unet.arp.getAddress(id)
-  if unet.driver.inter[id]
+  if unet.driver.inter[id] and unet.driver.inter[id].isAvailable then
+    if unet.driver.inter[id].parent == "0" then
+    
+    end
+  end
 end
 
 function unet.arp.checkAddr(addr)
