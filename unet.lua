0c6d5b6cf9eeea0c1ec57fec4325038211f2f247
stjohnd <devon.t.st.john@gmail.com>
2018-05-17 19:27:37 -0400

    Basic build of Unet API

--- unet.lua
+++ unet.lua
@@ -1,86 +1,41 @@
-local component = require("component")
-local computer = require("computer")
+--Unet V2 Library, the core API.
+
 local event = require("event")
-local fs = require("filesystem")
-local pack = require("serialization")
+unet = {interfaces = {}}
 
-local unet = {}
 
-unet.info = {   --version info, used in preloading and version control DO NOT EDIT
-["gateway"] = 1, ["inter"] = {},
-["os_build"] = "OpenOS 1.5",
-["version"] = "1.0.0 Beta",
-["allowOutdated"] = false,
-["mods"] = "/unet/mods",
-["config"] = "/unet/config.cfg"
+unet.utils = {
+  isSameSubnet = function(first,second,mask)
+    return (first & mask) == (second & mask)
+  end,
+  getBroadcastAddr = function(interface)
+    return (~interface.subnet) | interface.addr
+  end
 }
 
-function saveConfig()
-  local toSave = {["gateway"] = unet.info.gateway,["inter"] = unet.info.inter}
-  
-  for k,v in pairs(unet.info.inter) do
-    if not v.static then
-      toSave[k].addr = ""
+unet.send = function(source,dest,proto,data)
+  if type(proto) == "string" and data == nil then 
+    data = proto
+    proto = dest
+    dest = source
+    
+    for k,v in pairs(unet.interfaces) do
+      if v.addr == dest then
+        event.push("unet_packet",dest,dest,proto,data)
+      elseif unet.utils.isSameSubnet(v.addr,dest,v.subnet) then
+        return v:send(dest,proto,data)
+      end
     end
-  end
+    
+  else
   
-  local file = fs.open(unet.info.config,"w")
-  file.write(pack.serialize(toSave))
-  file.close()
-  computer.pushSignal("unet_config_saved")
-end
-
-
-function loadConfig()
-  if not fs.exists(unet.info.config) then
-    saveConfig()
-  end
-  
-  local file = fs.open(unet.info.config)
-  local toLoad = file.read(math.huge)
-  file.close()
-  
-  unet.info.gateway = toLoad.gateway
-  unet.info.inter = toLoad.inter
-  computer.pushSignal("unet_config_loaded")
-end
-
-
-function unet.getName()
-  if _DEVICENAME then 
-    return _DEVICENAME 
-  else return 
-    string.sub(computer.address(),1,5).."..."
-  end
-end
-
-function unet.setGateway(id)
-  if unet.driver.inter[id].isAvailible then
-    unet.info.gateway = id
-    unet.saveConfig()
-    return true
-  end
-  return false
-end
-
-function unet.loadMod(name)
-  if unet[name] then
-    return true,"Mod already loaded into memory"
-  elseif fs.exists(unet.info.mods.."/"..name..".lua") then
-    dofile(unet.info.mods.."/"..name..".lua")
-    if unet[name] then
-      return true,"Mod loaded into unet"
-    else
-      return false,"Mod failed to load"
+    for k,v in pairs(unet.interfaces) do
+      if v.addr == source then
+        return v:send(dest,proto,data)
+      end
     end
+    
   end
-end
-
-function unet.transmit(dest,ttl,packetType,data,inter)
-  if not inter then
-    inter = unet.info.gateway
-  end
-  
 end
 
 return unet
